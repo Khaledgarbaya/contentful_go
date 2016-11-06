33ce98bd4e68688f0916b9a5018a90585e184e6c
Khaledgarbaya <khaledgarbaya@gmail.com>
2016-11-06 14:46:53 +0100

    fixup! change folder structure

--- entry.go
+++
@@ -1,8 +0,0 @@
-package contentful
-
-import "github.com/Khaledgarbaya/contentful/entities"
-
-type Entry struct {
-	Sys    entities.Sys           `json:"sys"`
-	Fields map[string]interface{} `json:"fields"`
-}
--- space.go
+++
@@ -1,7 +0,0 @@
-package contentful
-
-import "github.com/Khaledgarbaya/contentful/entities"
-
-type Space struct {
-	Sys entities.Sys
-}
