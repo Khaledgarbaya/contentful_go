3c0736ca144a88b8ee2081bd780fe5153c475124
Khaledgarbaya <khaledgarbaya@gmail.com>
2016-12-11 00:45:02 +0100

    Code Clean up

--- entry.go
+++ entry.go
@@ -2,6 +2,7 @@ package contentful_go
 
 import "github.com/Khaledgarbaya/contentful_go/entities"
 
+// Entry a complete definition of an entry
 type Entry struct {
 	Sys    entities.Sys           `json:"sys"`
 	Fields map[string]interface{} `json:"fields"`
